e08635c25ede8b4bdb55de00b4c2c92f7c3741f5
Yash Haldiya <yashasvi9199@gmail.com>
2025-11-20 14:16:15 +0530

    fix: incorrect column name

--- api/fitness/create-routine.js
+++ api/fitness/create-routine.js
@@ -10,7 +10,7 @@ export default async function handler(req, res){
     }
 
     try{
-        const {user_id, name, exercise } = req.body;
+        const {user_id, name, exercises } = req.body;
 
         const {data, error} = await supabase
             .from('fitness_routines')
