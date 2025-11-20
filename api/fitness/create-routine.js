4e9eec8ea50df5b79a2afb0655bdb93baa4bca76
Yash Haldiya <yashasvi9199@gmail.com>
2025-11-20 14:00:14 +0530

    fix: supabase url

--- api/fitness/create-routine.js
+++ api/fitness/create-routine.js
@@ -1,7 +1,7 @@
 const {createClient} = require('@supabase/supabase-js');
 
 const supabase = createClient(
-    process.env.CITE_SUPABASE_URL,
+    process.env.VITE_SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export default async function handler(rq, res){
